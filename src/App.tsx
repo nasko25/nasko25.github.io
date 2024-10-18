64c21f3c5976f2ace9e89d57397660e2d752a189
nasko25 <apashov0@gmail.com>
2024-10-18 20:12:06 +0300

    feat: icons and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -86,41 +86,6 @@ function App() {
       <Physics timeStep="vary">
         <Ghost />
       </Physics>
-      {/* <Environment files="/night.hdr" ground={{ scale: 100 }} />
-        <directionalLight
-          intensity={0.7}
-          castShadow
-          shadow-bias={-0.0004}
-          position={[-20, 20, 20]}
-        >
-          <orthographicCamera
-            attach="shadow-camera"
-            args={[-20, 20, 20, -20]}
-          />
-        </directionalLight>
-        <ambientLight intensity={0.2} />
-        <Physics timeStep="vary">
-          <KeyboardControls map={keyboardMap}>
-            <Controller maxVelLimit={5}>
-              <Gltf
-                castShadow
-                receiveShadow
-                scale={0.315}
-                position={[0, -0.55, 0]}
-                src="/ghost_w_tophat-transformed.glb"
-              />
-            </Controller>
-          </KeyboardControls>
-          <RigidBody type="fixed" colliders="trimesh">
-            <Gltf
-              castShadow
-              receiveShadow
-              rotation={[-Math.PI / 2, 0, 0]}
-              scale={0.11}
-              src="/fantasy_game_inn2-transformed.glb"
-            />
-          </RigidBody>
-        </Physics> */}
       {/* </Fisheye> */}
       <>
         <ambientLight intensity={0.8} />
